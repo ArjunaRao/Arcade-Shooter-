8239390acae550f984b78bbb0af969e75adc03a9
Arjun Rao <coocooarj@yahoo.com>
2014-10-06 17:26:24 -0400

    Update enemy1.cs
    I accidentally put a different script in here.  This is the actually enemy script

--- enemy1.cs
+++ enemy1.cs
@@ -1,22 +1,39 @@
 using UnityEngine;
 using System.Collections;
 
-public class camera : MonoBehaviour {
+public class enemy1 : MonoBehaviour {
 
-	public Transform target;
-	public float distance;
-	public float lockedX = 0;
+	// Inspector Variables
+	public float speed = 6.0f;
+	public Transform player;
+	public Transform explosion;
+	public AudioClip pdeath;
+
+	// Private Variables
 
 	// Use this for initialization
 	void Start () {
-	
+		player = GameObject.Find ("pref_Player").transform;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		// currently, once an enemy is spwaned it will stupidly just move towards the player
 		// this code needs to change DRASTICALLY in order to make for interesting and fun gameplay
-		transform.position = new Vector3 (target.transform.position.x, target.transform.position.y,
-		                                  target.transform.position.z -distance);
+		transform.LookAt (player.transform, Vector3.back);
+		transform.position += transform.forward * speed * Time.deltaTime;
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag == "player") {
+			Instantiate (explosion,transform.position, transform.rotation);
+			AudioSource.PlayClipAtPoint (pdeath, transform.position);
+
+
+			// other.GetComponent("script name").variable etc
+
+			//Destroy (other.gameObject);
+			Destroy (gameObject);
+		}
 	}
 }
